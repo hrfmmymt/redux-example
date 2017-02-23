35781ada0118d67c7fac49881e20246cb0399792
hrfmmymt <hrfmmymt@gmail.com>
2017-02-24 00:18:43 +0900

    router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import 'babel-polyfill'
 import React from 'react'
 import { Provider } from 'react-redux'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
 
 import configureStore from './store/configureStore'
+import App from './containers/App'
+import Home from './containers/Home'
 import Posts from './containers/Posts'
 
 const store = configureStore()
+const history = syncHistoryWithStore(browserHistory, store)
+
+const NotFound = () => (<div><span>NOT FOUND</span></div>)
 
 export default (props) => (
   <Provider store={store}>
-    <Posts />
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Home} />
+        <Route path="posts" component={Posts} />
+      </Route>
+      <Route path="*" component={NotFound}/>
+    </Router>
   </Provider>
 )
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux'
+import { routerReducer } from 'react-router-redux'
+
 import { FETCH_SUCCESSED } from '../actions'
 
-const initialState = { 
+const fetchInitialState = {
   posts: []
 }
 
-function fetch(state = initialState, action) {
+function fetch(state = fetchInitialState, action) {
   switch (action.type) {
   case FETCH_SUCCESSED:
     return Object.assign({}, state, action.payload)
@@ -16,6 +18,7 @@ function fetch(state = initialState, action) {
 
 const rootReducer = combineReducers({
   fetch,
+  routing: routerReducer,
 })
 
 export default rootReducer
